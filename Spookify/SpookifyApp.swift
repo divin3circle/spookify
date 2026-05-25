26a09cf5369a258d77b1b9dae6dec4964e664db3
divin3circle <sylus.abel@icloud.com>
2026-05-25 06:53:34 +0300

    Initial Commit

---
+++ Spookify/SpookifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpookifyApp.swift
+//  Spookify
+//
+//  Created by Sylus Abel on 25/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct SpookifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpookifyTests/SpookifyTests.swift
@@ -0,0 +1,19 @@
+//
+//  SpookifyTests.swift
+//  SpookifyTests
+//
+//  Created by Sylus Abel on 25/05/2026.
+//
+
+import Testing
+@testable import Spookify
+
+struct SpookifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
